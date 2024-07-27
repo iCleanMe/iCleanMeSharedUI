f96fad9eb71b0c91b68fac03229329f6db754bd2
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-07-27 14:57:13 -0700

    updated to allow NavStack to be built in macOS

--- Sources/iCleanMeSharedUI/ViewStyles/TextField/CleanFieldStyle.swift
+++ Sources/iCleanMeSharedUI/ViewStyles/TextField/CleanFieldStyle.swift
@@ -5,18 +5,22 @@
 //  Created by Nikolai Nobadi on 7/5/24.
 //
 
-#if canImport(UIKit)
 import SwiftUI
 
 public struct CleanFieldStyle: TextFieldStyle {
     let alignment: TextAlignment
+    
+    #if canImport(UIKit)
     let keyboardType: UIKeyboardType
+    #endif
     
     public func _body(configuration: TextField<Self._Label>) -> some View {
         configuration
             .tint(.blue)
+        #if canImport(UIKit)
             .keyboardType(keyboardType)
             .autocapitalization(.none)
+        #endif
             .autocorrectionDisabled(true)
             .textFieldStyle(.roundedBorder)
             .multilineTextAlignment(alignment)
@@ -24,8 +28,13 @@ public struct CleanFieldStyle: TextFieldStyle {
 }
 
 public extension TextFieldStyle where Self == CleanFieldStyle {
+    #if canImport(UIKit)
     static func cleanFieldStyle(alignment: TextAlignment = .center, keyboardType: UIKeyboardType = .alphabet) -> CleanFieldStyle {
         return .init(alignment: alignment, keyboardType: keyboardType)
     }
+    #else
+    static func cleanFieldStyle(alignment: TextAlignment = .center) -> CleanFieldStyle {
+        return .init(alignment: alignment)
+    }
+    #endif
 }
-#endif
