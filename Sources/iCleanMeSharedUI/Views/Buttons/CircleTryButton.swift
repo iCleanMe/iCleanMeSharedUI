c4cde6eca8defba8a79bb90e447d0eb7e5d01a23
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-08-10 16:43:01 -0700

    added CircleTryButton

---
+++ Sources/iCleanMeSharedUI/Views/Buttons/CircleTryButton.swift
@@ -0,0 +1,39 @@
+//
+//  CircleTryButton.swift
+//
+//
+//  Created by Nikolai Nobadi on 8/10/24.
+//
+
+import SwiftUI
+import NnSwiftUIKit
+
+public struct CircleTryButton: View {
+    let accessibilityId: String?
+    let action: () throws -> Void
+    
+    public init(accessibilityId: String? = nil, action: @escaping () -> Void) {
+        self.accessibilityId = accessibilityId
+        self.action = action
+    }
+    
+    private var buttonSize: CGFloat {
+        #if canImport(UIKit)
+        return getHeightPercent(isSmallPhone ? 8 : 6)
+        #else
+        return getHeightPercent(6)
+        #endif
+    }
+    
+    public var body: some View {
+        TryButton(action: action) {
+            Image(systemName: "plus")
+                .withFont(.title3, textColor: .white)
+        }
+        .nnSetAccessibiltyId(accessibilityId)
+        .frame(maxWidth: buttonSize, maxHeight: buttonSize)
+        .withGradientBackground(.seaNight)
+        .clipShape(Circle())
+        .shadow(color: .primary, radius: 7)
+    }
+}
